66fae2596d6bdfc2423d22b4cd60af57106d51fb
LeHoangMinhQuan <quanb2206008@student.ctu.edu.vn>
2026-08-01 13:07:07 +0700

    fix: add missing variable

--- be/src/config/env.ts
+++ be/src/config/env.ts
@@ -114,6 +114,7 @@ export const env = {
   FIREBASE_PROJECT_ID: process.env["FIREBASE_PROJECT_ID"],
   FIREBASE_CLIENT_EMAIL: process.env["FIREBASE_CLIENT_EMAIL"],
   FIREBASE_WEB_API_KEY: process.env["FIREBASE_WEB_API_KEY"],
+  FIREBASE_PRIVATE_KEY: process.env["FIREBASE_PRIVATE_KEY"],
 
   // ─── SMTP (forgot-password only — optional, see note above) ────────────────
   SMTP_HOST: process.env["SMTP_HOST"],
